0a8a322b4af9468eabb205b4175d0f78b89c0a94
newo-2001 <o.elderbroek@gmail.com>
2023-11-10 17:13:20 +0100

    Analog input blink speed

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use esp_idf_svc::hal::{prelude::Peripherals, gpio::PinDriver, delay::FreeRtos};
+use std::cmp::min;
+
+use esp_idf_svc::hal::{prelude::Peripherals, gpio::PinDriver, delay::FreeRtos, adc::{self, AdcDriver, AdcChannelDriver, attenuation}};
 use log::info;
 use anyhow::Result;
 
@@ -9,10 +11,32 @@ fn main() -> Result<()> {
     info!("Hello, world!");
 
     let peripherals = Peripherals::take()?;
+    
     let mut led = PinDriver::output(peripherals.pins.gpio12)?;
+    
+    let mut adc = AdcDriver::new(peripherals.adc2, &adc::config::Config::new().calibration(true))?;
+    let mut adc_pin: AdcChannelDriver<{ attenuation::DB_11 }, _> = AdcChannelDriver::new(peripherals.pins.gpio14)?;
 
     loop {
-        FreeRtos::delay_ms(500);
+        const MIN_BLINK_DELAY: u32 = 100;
+        const MAX_BLINK_DELAY: u32 = 500;
+        const BLINK_RANGE: u32 = MAX_BLINK_DELAY - MIN_BLINK_DELAY;
+
+        const MIN_SENSOR: u16 = 500;
+        const MAX_SENSOR: u16 = 3000;
+        const SENSOR_RANGE: u16 = MAX_SENSOR - MIN_SENSOR;
+
+        let value = adc.read(&mut adc_pin)?;
+        info!("value: {value}");
+
+        let value = min(value.saturating_sub(MIN_SENSOR), SENSOR_RANGE);
+        
+        #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss, clippy::cast_precision_loss)]
+        let delay = MIN_BLINK_DELAY + (f32::from(value) / f32::from(SENSOR_RANGE) * BLINK_RANGE as f32) as u32;
+
+        info!("delay: {delay}");
+
+        FreeRtos::delay_ms(delay);
 
         led.toggle()?;
     }
